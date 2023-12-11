aa0174885edb7ade24ff86d876e432c65ccbd6d1
Tn-E235 <tne235bve@gmail.com>
2023-12-11 22:22:46 +0900

    モニタプラグインベース作成

---
+++ interos_mon/main.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using AtsEx.PluginHost;
+using AtsEx.PluginHost.Handles;
+using AtsEx.PluginHost.Plugins;
+
+using BveTypes.ClassWrappers;
+
+namespace AtsExCsTemplate.VehiclePlugin {
+    [PluginType(PluginType.VehiclePlugin)]
+    internal class VehiclePluginMain : AssemblyPluginBase {
+        public VehiclePluginMain (PluginBuilder builder) : base(builder) {
+        }
+
+        public override void Dispose () {
+        }
+
+        public override TickResult Tick (TimeSpan elapsed) {
+            return new VehiclePluginTickResult();
+        }
+
+    }
+}
---
+++ interos_mon/monitor.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Zbx1425.DXDynamicTexture;
+
+using AtsEx.PluginHost.Plugins;
+using AtsEx.PluginHost;
+using BveTypes.ClassWrappers;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
+
+namespace AtsExCsTemplate.VehiclePlugin {
+    [PluginType(PluginType.VehiclePlugin)]
+    public class DXDynamicTextureTest : AssemblyPluginBase {
+        private GDIHelper gdi;
+        private Bitmap mon_R;
+        private static TextureHandle txH;
+
+        private const String DEFAULT_FONT_FAMILY = "Cica";// "HGSｺﾞｼｯｸE"; //"KHドット秋葉原16";//  "JFドットjiskan16";
+        private const String DEFAULT_DOT_FONT_FAMILY = "JFドットAyuゴシック20";//"JFドットjiskan16";
+        private const int MON_H = 800;
+        private const int MON_W = 1280;
+        private bool update = true;
+        private static string dllParentPath = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+
+        public DXDynamicTextureTest (PluginBuilder builder) : base(builder) {
+            BveHacker.ScenarioCreated += OnScenarioCreated;
+            txH = TextureManager.Register("base.png", 2048, 1024);
+            gdi = new GDIHelper(txH.Width, txH.Height);
+            mon_R = new Bitmap(Path.Combine(dllParentPath, "./tex/base.png"));
+        }
+
+        public override void Dispose () {
+            BveHacker.ScenarioCreated -= OnScenarioCreated;
+            mon_R.Dispose();
+        }
+
+
+        private void OnScenarioCreated (ScenarioCreatedEventArgs e) {
+        }
+
+        public override TickResult Tick (TimeSpan elapsed) {
+
+            if (txH.IsCreated && txH.HasEnoughTimePassed(10) && update) {
+
+                Rectangle rc = new Rectangle(0, 0, 1280, 800);
+                gdi.Graphics.FillRectangle(new SolidBrush(Color.FromArgb(71, 80, 101)), rc);
+
+                gdi.BeginGDI();
+                gdi.EndGDI();
+                
+                txH.Update(gdi);
+                update = false;
+            }
+
+            return new VehiclePluginTickResult();
+        }
+    }
+}
+
+
+
+
+
+
+
+
