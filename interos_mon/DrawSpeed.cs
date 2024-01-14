bf60237b2ab3b405983f4353f9b836859b9a0494
Tn-E235 <tne235bve@gmail.com>
2024-01-14 17:26:04 +0900

    現在速度表示を実装

--- interos_mon/DrawSpeed.cs
+++ interos_mon/DrawSpeed.cs
@@ -1,35 +1,68 @@
 ﻿using PITempCS.mon;
+using SlimDX.DirectSound;
 using System;
 using System.Drawing;
-using System.Runtime.CompilerServices;
-using Zbx1425.DXDynamicTexture;
 
-public class DrawSpeed {
-	private DRAW_INF_SET draw_inf;
+public class MakeSpeedImg {
+	DWST_RECT_TEXT draw_inf;
 	int speed;
+    Bitmap bmp;
+    Graphics g;
+	bool update;
 
-    public DrawSpeed(int x, int y, int size, Color color, String font, Color bg) {
-		draw_inf = new DRAW_INF_SET(x, y, size, color, font, bg);
-		this.speed = 0;
+    public MakeSpeedImg(int x, int y, int size, Color color, String font, Color bg) {
+		this.draw_inf = new DWST_RECT_TEXT(x, y, size, color, font, bg);
+		this.speed = -1;
+		this.update = false;
+		this.bmp = new Bitmap(150, 30);
+		this.g = Graphics.FromImage(this.bmp);
+        this.g.DrawString("km/h", this.draw_inf.font, new SolidBrush(this.draw_inf.color), 4 * 16, 0);
+    }
+
+	public void Dispose () {
+		this.g.Dispose();
 	}
 
-	public void Draw (GDIHelper i_gdi, int i_speed) {
+	public Bitmap get () {
+		return this.bmp;
+	}
+
+	public bool isUpdate () {
+		return this.update;
+	}
+
+	public void make (int i_speed) {
+		
+		// 描画更新判定
 		if (this.speed == i_speed) {
+			this.update = false;
 			return;
 		}
 
-		if (this.speed / 100 != i_speed / 100) {
-            i_gdi.Graphics.FillRectangle(new SolidBrush(this.draw_inf.bg), new Rectangle(this.draw_inf.x, this.draw_inf.y, 50, 30));
-        }
+        this.g.FillRectangle(new SolidBrush(this.draw_inf.bg), new Rectangle(0, 0, 16 * 3 + 8, 30));
+        this.draw(0, i_speed);
+        this.draw(1, i_speed);
+        this.draw(2, i_speed);
+        this.speed = i_speed;
+		this.update = true;
+	}
 
-		if (this.speed / 10 % 10 == i_speed / 10 % 10) {
-            i_gdi.Graphics.FillRectangle(new SolidBrush(this.draw_inf.bg), new Rectangle(this.draw_inf.x, this.draw_inf.y, 50, 30));
-        }
-
-		if (this.speed % 10 == i_speed % 10) {
-            i_gdi.Graphics.FillRectangle(new SolidBrush(this.draw_inf.bg), new Rectangle(this.draw_inf.x, this.draw_inf.y, 50, 30));
-        }
-
-		this.speed = i_speed;
+	private void fill (int i_x) {
+        this.g.FillRectangle(new SolidBrush(this.draw_inf.bg), new Rectangle(i_x*16+8, 0, 16, 30));
+    }
+	private void draw (int i_keta, int i_speed) {
+		int num = 0;
+		if (i_keta == 0) {
+			num = i_speed / 100;
+			if (i_speed < 100) return;
+		} else if (i_keta == 1) {
+			num = i_speed / 10 % 10;
+            if (i_speed < 10) return;
+        } else if (i_keta == 2) {
+			num = i_speed % 10;
+		} else {
+			return;
+		}
+		this.g.DrawString(num.ToString(), this.draw_inf.font, new SolidBrush(this.draw_inf.color), i_keta*16, 0);
 	}
 }
